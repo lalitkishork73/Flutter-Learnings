cc1b3c4c456971bd3861bec07b285b2b936b5744
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-04-13 11:39:40 +0530

    BMI app done

--- lib/main.dart
+++ lib/main.dart
@@ -37,73 +37,97 @@ var InInput=TextEditingController();
 
 var Result='';
 
+var BgColor=Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BMI'),
       ),
-      body:Column(
-        children: [
-          Text("BMI",style: TextStyle(fontSize: 35,
-              fontWeight: FontWeight.w700),),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: TextField(controller: WtInput,
-            decoration: InputDecoration(
-                label: Text('Enter Your Weight'),
-                prefixIcon: Icon(Icons.line_weight)
-            ),
-            keyboardType: TextInputType.number,
-            ),
-          ),Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: TextField(controller: FtInput,
-            decoration: InputDecoration(
-                label: Text('Enter Your Height'),
-                prefixIcon: Icon(Icons.height)
-            ),
-            keyboardType: TextInputType.number,
-            ),
-          ),Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: TextField(controller: InInput,
-            decoration: InputDecoration(
-                label: Text('Enter Your Height in (Inch)'),
-                prefixIcon: Icon(Icons.height )
-            ),
-            keyboardType: TextInputType.number,
+      body:SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Center(
+          child: Container(
+
+            color: BgColor,
+            child: Column(
+
+              children: [
+                Text("BMI",style: TextStyle(fontSize: 35,
+                    fontWeight: FontWeight.w700),),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: TextField(controller: WtInput,
+                  decoration: InputDecoration(
+                      label: Text('Enter Your Weight'),
+                      prefixIcon: Icon(Icons.line_weight)
+                  ),
+                  keyboardType: TextInputType.number,
+                  ),
+                ),Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: TextField(controller: FtInput,
+                  decoration: InputDecoration(
+                      label: Text('Enter Your Height'),
+                      prefixIcon: Icon(Icons.height)
+                  ),
+                  keyboardType: TextInputType.number,
+                  ),
+                ),Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: TextField(controller: InInput,
+                  decoration: InputDecoration(
+                      label: Text('Enter Your Height in (Inch)'),
+                      prefixIcon: Icon(Icons.height )
+                  ),
+                  keyboardType: TextInputType.number,
+                  ),
+                ),
+                ElevatedButton(onPressed: (){
+                  var wt=WtInput.text.toString();
+                  var ft=FtInput.text.toString();
+                  var In=InInput.text.toString();
+
+                  if(wt!="" && ft!="" && In!=""){
+                      var Iwt=int.parse(wt);
+                      var Ift=int.parse(ft);
+                      var Iin=int.parse(In);
+
+                      var tInch=(Ift*12)+Iin;
+                      var tCm=tInch*2.54;
+                      var tM=tCm/100;
+
+                      var bmi=Iwt/(tM*tM);
+                      var msg='';
+                      if(bmi>=25){
+                        msg='You are OverWeight';
+                        BgColor=Colors.yellowAccent.shade100;
+
+                      }else if(bmi<=18){
+                        msg='You are UnderWeight';
+                        BgColor=Colors.redAccent.shade100;
+                      }else{
+                        msg='You are Healthy!!';
+                        BgColor=Colors.green.shade50;
+                      }
+                      setState(() {
+                        Result="$msg \n Your BMI is: ${bmi.toString().substring(0,5)}";
+                      });
+
+                  }else{
+                    setState(() {
+                      Result="Please Fill all Required Line";
+                    });
+
+                  }
+
+                }, child: Text('Calculate')),
+                Text('  $Result',style: TextStyle(fontSize: 15,),),
+              ],
             ),
           ),
-          ElevatedButton(onPressed: (){
-            var wt=WtInput.text.toString();
-            var ft=FtInput.text.toString();
-            var In=InInput.text.toString();
-
-            if(wt!="" && ft!="" && In!=""){
-                var Iwt=int.parse(wt);
-                var Ift=int.parse(ft);
-                var Iin=int.parse(In);
-
-                var tInch=(Ift*12)+Iin;
-                var tCm=tInch*2.54;
-                var tM=tCm/100;
-
-                var bmi=Iwt/(tM*tM);
-                setState(() {
-                  Result=bmi.toString().substring(0,5);
-                });
-
-            }else{
-              setState(() {
-                Result="Please Fill all Required Line";
-              });
-
-            }
-
-          }, child: Text('Calculate')),
-          Text('  $Result',style: TextStyle(fontSize: 15,),),
-        ],
+        ),
       )
 
     );
